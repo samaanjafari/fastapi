e71606adfda496361c43c5d3544e6d96a5de5552
saman jafari <samanjfr81@gmail.com>
2024-06-02 14:36:52 +0330

    hashed passwords.

--- app/main.py
+++ app/main.py
@@ -1,3 +1,4 @@
+from typing_extensions import deprecated
 import psycopg2
 from psycopg2.extras import RealDictCursor
 from typing import Optional, List
@@ -6,8 +7,9 @@ from pydantic import BaseModel
 from random import randrange
 import time
 from sqlalchemy.orm import Session
-from .  import models, schemas
+from .  import models, schemas, utils
 from .database import engine, get_db
+
 
 models.Base.metadata.create_all(bind=engine)
 
@@ -127,6 +129,10 @@ def update_post(id: int , updated_post:schemas.PostCreate, db: Session = Depends
 
 @app.post("/users", status_code=status.HTTP_201_CREATED, response_model= schemas.UserOut)
 def create_user(user: schemas.UserCreate,db: Session = Depends(get_db)):
+    # hash the password - user password
+    hashed_password = utils.hash(user.password)
+    user.password = hashed_password
+    
     new_user = models.User(**user.model_dump())
     db.add(new_user)
     db.commit()
