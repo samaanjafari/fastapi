5a2d538954ac616c3582571bf91cba39c0e2a42c
saman jafari <samanjfr81@gmail.com>
2024-06-08 12:01:44 +0330

    routers implementation

--- app/main.py
+++ app/main.py
@@ -9,6 +9,7 @@ import time
 from sqlalchemy.orm import Session
 from .  import models, schemas, utils
 from .database import engine, get_db
+from .routers import user, post
 
 
 models.Base.metadata.create_all(bind=engine)
@@ -29,112 +30,12 @@ while True:
         print("Error is:", error)
         time.sleep(4)
            
+app.include_router(post.router)
+app.include_router(user.router)
+ 
     
 @app.get("/")
 def root():
     return {"message": "Welcome to my API"}
 
 
-@app.get("/posts", response_model= List[schemas.Response])
-def get_posts(db: Session = Depends(get_db)):
-    # cursor.execute("""SELECT * FROM posts""")
-    # posts = cursor.fetchall()
-    #print(posts)
-    posts = db.query(models.Post).all()
-    return  posts
-
- 
-@app.post("/posts", status_code=status.HTTP_201_CREATED, response_model=schemas.Response)
-def create_post(post : schemas.PostCreate, db: Session = Depends(get_db)):   
-    # cursor.execute("""INSERT INTO posts (title, content, published) VALUES (%s, %s, %s) RETURNING *  """
-    #                , (post.title, post.content, post.published))
-    # new_post = cursor.fetchone()
-    # conn.commit()
-    print(post)
-    new_post = models.Post(**post.model_dump())
-    '''we are using post.dict() and we unpack it with **kwargs method and it automatically fill our
-    need columns and put it into right place and we do not have to fill it manually anymore every
-    column'''
-    db.add(new_post)
-    db.commit()
-    db.refresh(new_post)
-    return  new_post
-
-
-@app.get("/posts/latest")
-def get_latest():
-    cursor.execute("""SELECT * FROM posts ORDER BY created_at  DESC LIMIT 1;""")
-    post = cursor.fetchone()
-    return post
-
-
-
-
-@app.get("/posts/{id}", response_model= schemas.Response)
-def get_post(id: int , db: Session = Depends(get_db)):
-    # cursor.execute("""SELECT * FROM posts WHERE id = (%s) """ , (str(id),))
-    '''the comma after str(id), is for if we don't pass that comma it doesn't different from
-    a single regular variable in parentheses and we have to pass tuple or a list to avoid 
-    sql injection so we have to pass that comma to specify thay it is a single tuple.'''
-    # post = cursor.fetchone()
-    #print(post)
-    post = db.query(models.Post).filter(models.Post.id == id).first()
-
-
-    if not post:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                            detail=f"post with id: {id} was not found.")
-
-    return post 
-
-
-@app.delete("/posts/{id}" , status_code=status.HTTP_204_NO_CONTENT)
-def delete_post(id: int, db: Session = Depends(get_db)):
-
-    # cursor.execute("""DELETE FROM posts WHERE id = %s RETURNING *"""
-    #                , (str(id),))
-    # deleted_post = cursor.fetchone()
-    # conn.commit()
-    post = db.query(models.Post).filter(models.Post.id == id)
-    
-    if post.first()==None:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                            detail=f"post with id {id} does not exist.")
-        
-    post.delete()    
-    db.commit()   
-    
-    return Response(status_code=status.HTTP_204_NO_CONTENT)
-
-
-@app.put("/posts/{id}", response_model= schemas.Response)
-def update_post(id: int , updated_post:schemas.PostCreate, db: Session = Depends(get_db)):
-    
-    # cursor.execute("""UPDATE posts SET title = %s, content =  %s, published = %s WHERE id = %s RETURNING *"""
-    #                , (post.title, post.content, post.published, (str(id))))
-    # updated_post = cursor.fetchone()
-    # conn.commit()
-    post_query = db.query(models.Post).filter(models.Post.id == id)
-    post = post_query.first()
-    
-    if post == None:
-    # really important lesson that i get here is if not index is true even index==0 and we should use NONE here.
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                            detail=f"post with id {id} does not exist.")
-    post_query.update(updated_post.model_dump())
-    
-    db.commit()
-       
-    return  post_query.first()
-
-@app.post("/users", status_code=status.HTTP_201_CREATED, response_model= schemas.UserOut)
-def create_user(user: schemas.UserCreate,db: Session = Depends(get_db)):
-    # hash the password - user password
-    hashed_password = utils.hash(user.password)
-    user.password = hashed_password
-    
-    new_user = models.User(**user.model_dump())
-    db.add(new_user)
-    db.commit()
-    db.refresh(new_user)
-    return  new_user
---
+++ app/routers/post.py
@@ -0,0 +1,90 @@
+from .. import models, schemas
+from fastapi import Body, FastAPI , Response , status , HTTPException, Depends, APIRouter
+from ..database import  get_db
+from sqlalchemy.orm import Session
+from typing import List
+
+router = APIRouter()
+
+@router.get("/posts", response_model= List[schemas.Response])
+def get_posts(db: Session = Depends(get_db)):
+    # cursor.execute("""SELECT * FROM posts""")
+    # posts = cursor.fetchall()
+    #print(posts)
+    posts = db.query(models.Post).all()
+    return  posts
+
+ 
+@router.post("/posts", status_code=status.HTTP_201_CREATED, response_model=schemas.Response)
+def create_post(post : schemas.PostCreate, db: Session = Depends(get_db)):   
+    # cursor.execute("""INSERT INTO posts (title, content, published) VALUES (%s, %s, %s) RETURNING *  """
+    #                , (post.title, post.content, post.published))
+    # new_post = cursor.fetchone()
+    # conn.commit()
+    print(post)
+    new_post = models.Post(**post.model_dump())
+    '''we are using post.dict() and we unpack it with **kwargs method and it automatically fill our
+    need columns and put it into right place and we do not have to fill it manually anymore every
+    column'''
+    db.add(new_post)
+    db.commit()
+    db.refresh(new_post)
+    return  new_post
+
+
+@router.get("/posts/{id}", response_model= schemas.Response)
+def get_post(id: int , db: Session = Depends(get_db)):
+    # cursor.execute("""SELECT * FROM posts WHERE id = (%s) """ , (str(id),))
+    '''the comma after str(id), is for if we don't pass that comma it doesn't different from
+    a single regular variable in parentheses and we have to pass tuple or a list to avoid 
+    sql injection so we have to pass that comma to specify thay it is a single tuple.'''
+    # post = cursor.fetchone()
+    #print(post)
+    post = db.query(models.Post).filter(models.Post.id == id).first()
+
+
+    if not post:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"post with id: {id} was not found.")
+
+    return post 
+
+
+@router.delete("/posts/{id}" , status_code=status.HTTP_204_NO_CONTENT)
+def delete_post(id: int, db: Session = Depends(get_db)):
+
+    # cursor.execute("""DELETE FROM posts WHERE id = %s RETURNING *"""
+    #                , (str(id),))
+    # deleted_post = cursor.fetchone()
+    # conn.commit()
+    post = db.query(models.Post).filter(models.Post.id == id)
+    
+    if post.first()==None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"post with id {id} does not exist.")
+        
+    post.delete()    
+    db.commit()   
+    
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
+
+
+@router.put("/posts/{id}", response_model= schemas.Response)
+def update_post(id: int , updated_post:schemas.PostCreate, db: Session = Depends(get_db)):
+    
+    # cursor.execute("""UPDATE posts SET title = %s, content =  %s, published = %s WHERE id = %s RETURNING *"""
+    #                , (post.title, post.content, post.published, (str(id))))
+    # updated_post = cursor.fetchone()
+    # conn.commit()
+    post_query = db.query(models.Post).filter(models.Post.id == id)
+    post = post_query.first()
+    
+    if post == None:
+    # really important lesson that i get here is if not index is true even index==0 and we should use NONE here.
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"post with id {id} does not exist.")
+    post_query.update(updated_post.model_dump())
+    
+    db.commit()
+       
+    return  post_query.first()
